099b1df4b6cac51d7c8f1de6d8de37aa2bbabb3c
Marcelo150803 <130611204+Marcelo150803@users.noreply.github.com>
2023-04-12 20:40:12 -0300

    Add files via upload

---
+++ src/emprestdvd/Interface - Copia.java
@@ -0,0 +1,12 @@
+package emprestdvd;
+
+public interface Interface {
+     
+     public void imprime(String imprime);
+     public void imprimeMenu ();
+     public DVD leDVD();
+     public Amigo leAmigo();
+     public void leEmprestimo();
+     public void alertaFaixaEtaria(Amigo alertaFaixaEtariaA, DVD alertaFaixaEtariaD);
+    
+}
