aa67fbfe74d382fccea102f63dd7e13d21ae43f5
Lucas Almeida Coelho <89714517+lucasacoelho@users.noreply.github.com>
2023-04-12 21:20:51 -0300

    Add files via upload

---
+++ src/emprestdvd/CadastroAmigo.java
@@ -0,0 +1,14 @@
+package emprestdvd;
+
+public class CadastroAmigo {
+
+    public void inclui(Amigo inclui) {
+    }
+
+    public void altera(int altera) {
+    }
+
+    public void exclui(int exclui) {
+
+    }
+}
