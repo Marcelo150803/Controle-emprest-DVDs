8ce28790e12591a006a865f60e60aa76ff511555
Marcelo150803 <130611204+Marcelo150803@users.noreply.github.com>
2023-04-12 21:07:53 -0300

    Add files via upload

---
+++ src/emprestdvd/RegistroEmprestimo.java
@@ -0,0 +1,14 @@
+package emprestdvd;
+
+public class RegistroEmprestimo {
+    public void empresta(Amigo emprestaA, DVD emprestaD){
+    }
+    public void devolve(DVD devolve){
+    }
+    private boolean estahEmprestado(DVD estahEmprestado){
+        return true;
+    }
+    private boolean faixaEtariaAbaixo(Amigo faixaEtariaAbaixoA, DVD faixaEtariaAbaixoD){
+        return true;
+    }
+}
