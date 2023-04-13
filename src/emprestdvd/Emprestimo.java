e950b4d785a5c6cf48ca73f45fe6117c91dfe35d
Marcelo150803 <130611204+Marcelo150803@users.noreply.github.com>
2023-04-12 21:15:10 -0300

    Add files via upload

---
+++ src/emprestdvd/Emprestimo.java
@@ -0,0 +1,30 @@
+package emprestdvd;
+
+import java.util.Date;
+
+public class Emprestimo {
+    private Date dataEmprestimo;
+    private Date dataDevolver;
+
+    public Emprestimo(Date dataEmprestimo, Date dataDevolver) {
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolver = dataDevolver;
+    }
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolver() {
+        return dataDevolver;
+    }
+
+    public void setDataDevolver(Date dataDevolver) {
+        this.dataDevolver = dataDevolver;
+    }
+    
+}
